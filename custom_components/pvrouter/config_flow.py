6e5f8ad6e6d5ff8837e63c087199f4df7231db5d
nri04 <nri04.dev@gmail.com>
2026-02-27 15:11:59 +0100

    Update config_flow.py

--- custom_components/pvrouter/config_flow.py
+++ custom_components/pvrouter/config_flow.py
@@ -3,10 +3,13 @@ from homeassistant import config_entries
 from .const import DOMAIN, CONF_TOPIC_PREFIX
 
 class PvRouterConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for PvRouter NRI."""
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        """Etape initiale de configuration."""
         if user_input is not None:
+            # On crée l'entrée en utilisant la clé définie dans const.py
             return self.async_create_entry(
                 title=f"PvRouter ({user_input[CONF_TOPIC_PREFIX]})", 
                 data=user_input
