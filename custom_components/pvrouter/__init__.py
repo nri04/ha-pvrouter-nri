ede180257c6a9a73a2b15ce3682e8abb4a6173b3
nri04 <nri04.dev@gmail.com>
2026-02-27 15:54:22 +0100

    Update __init__.py

--- custom_components/pvrouter/__init__.py
+++ custom_components/pvrouter/__init__.py
@@ -1,17 +1,29 @@
 # -*- coding: utf-8 -*-
+import os
 import logging
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.components.frontend import add_extra_js_url
+from homeassistant.components.http import StaticPathConfig
 from .const import DOMAIN, CONF_TOPIC_PREFIX
 from .coordinator import PvRouterCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
 PLATFORMS = ["sensor", "switch", "button"]
+CARD_URL = "/pvrouter-nri/pvrouter-card.js"
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
+    # Enregistrement du fichier JS de la carte
+    www_path = hass.config.path("custom_components/pvrouter/www/pvrouter-card.js")
+    if os.path.isfile(www_path):
+        await hass.http.async_register_static_paths([
+            StaticPathConfig(CARD_URL, www_path, False)
+        ])
+        add_extra_js_url(hass, CARD_URL)
+        _LOGGER.debug("PvRouter card enregistree: %s", CARD_URL)
 
     prefix = entry.data.get(CONF_TOPIC_PREFIX)
     coordinator = PvRouterCoordinator(hass, prefix)
