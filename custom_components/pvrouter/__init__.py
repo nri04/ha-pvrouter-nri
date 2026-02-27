15cac1e215c6bdcf28b38f2358966a0d9e8ac0be
nri04 <nri04.dev@gmail.com>
2026-02-27 15:46:06 +0100

    Add files via upload

--- custom_components/pvrouter/__init__.py
+++ custom_components/pvrouter/__init__.py
@@ -14,7 +14,10 @@ PLATFORMS = ["sensor", "switch", "button"]
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     www_path = hass.config.path("custom_components/pvrouter/www")
     if os.path.isdir(www_path):
-        hass.http.register_static_path("/pvrouter-nri/static", www_path, True)
+        from homeassistant.components.http import StaticPathConfig
+        await hass.http.async_register_static_paths([
+            StaticPathConfig("/pvrouter-nri/static", www_path, True)
+        ])
 
     prefix = entry.data.get(CONF_TOPIC_PREFIX)
     coordinator = PvRouterCoordinator(hass, prefix)
